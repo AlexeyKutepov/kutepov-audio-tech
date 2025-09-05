a28d7d094bbc61a77bceeba3e9ccd7fdaac150e6
Alexey.Kutepov <Alexey.Kutepov@russianpost.ru>
2025-09-05 15:40:10 +0300

    sitemap

--- main/sitemap.py
+++ main/sitemap.py
@@ -21,7 +21,7 @@ class StaticSitemap(Sitemap):
     i18n=True
 
     def items(self):
-        return ['index', 'products', 'product_details', 'blog', 'contacts']
+        return ['index', 'products', 'blog', 'contacts']
 
     def location(self, item):
         return reverse(item)
